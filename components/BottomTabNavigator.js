bfa4721df587e524f86522d0ba52200f123911f9
mellabanca <mellabanca@gmail.com>
2022-07-21 22:06:10 -0300

    Aula70Vinicius caixas de texto

--- components/BottomTabNavigator.js
+++ components/BottomTabNavigator.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 import TransactionScreen from "../screens/Transaction";
 import SearchScreen from "../screens/Search";
@@ -11,7 +12,49 @@ export default class BottomTabNavigator extends Component {
     render(){
       return(
         <NavigationContainer>
-          <tab.Navigator>
+          <tab.Navigator
+          screenOptions={({ route })=>({
+            tabBarIcon: ({focused, color, size}) => {
+              let iconName;
+              if(route.name === "Transação"){
+                iconName = "book";
+              } else if(route.name === "Pesquisa"){
+                iconName = "search";
+              }
+              return(
+                <Ionicons
+                  name={iconName}
+                  size={size}
+                  color={color}
+                />
+              )
+            }
+          })}
+            tabBarOptions={{
+              activeTintColor: "black",
+              inactiveTintColor: "#FFFFFF",
+              style: {
+                height: 130,
+                borderTopWidth: 0,
+                backgroundColor: "#5653d4"
+              },
+              labelStyle: {
+                fontSize: 20,
+                fontFamily: "Rajdhani_600SemiBold"
+              },
+              labelPosition: "beside-icon",
+              tabStyle: {
+                marginTop: 25,
+                marginLeft: 10,
+                marginRight: 10,
+                borderRadius: 30,
+                borderWidth: 2,
+                alignItems: "center",
+                justifyContent: "center",
+                backgroundColor: "#5653d4"
+              }
+            }}
+          >
             <tab.Screen name = "Transação" component={TransactionScreen}/>
             <tab.Screen name = "Pesquisa" component={SearchScreen}/>
           </tab.Navigator>
--- screens/Transaction.js
+++ screens/Transaction.js
@@ -1,7 +1,11 @@
 import React, {Component} from "react";
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity, TextInput, ImageBackground, Image } from "react-native";
 import { BarCodeScanner } from "expo-barcode-scanner";
 import * as Permissions from "expo-permissions";
+
+const bgImage = require("../assets/background2.png");
+const appIcon = require("../assets/appIcon.png");
+const appName = require("../assets/appName.png");
 
 export default class TransactionScreen extends Component {
     constructor(props){
@@ -10,7 +14,9 @@ export default class TransactionScreen extends Component {
             domState: "normal",
             hasCameraPermissions: null,
             scanned: false,
-            scannedData: ""
+            scannedData: "",
+            bookId: "",
+            studentId: ""
         }
     }
 
@@ -25,34 +31,70 @@ export default class TransactionScreen extends Component {
     }
 
     handleBarCodeScanned = async ({type, data}) => {
-        this.setState({
-            scannedData: data,
-            domState: "normal",
-            scanned: true
-        })
+        const { domState } = this.state;
+    if(domState === "bookId"){
+      this.setState({
+        bookId: data,
+        domState: "normal",
+        scanned: true
+      });
+    } else if (domState === "studentId"){
+      this.setState({
+        studentId: data,
+        domState: "normal",
+        scanned: true
+      });
+    }
     }
 
     render(){
-        const {domState, hasCameraPermissions, scannedData, scanned} = this.state;
-        if(domState === "scanner"){
+        const {bookId, studentId, domState, hasCameraPermissions, scannedData, scanned} = this.state;
+        if(domState !== "normal"){
             return(
                 <BarCodeScanner onBarCodeScanned={scanned ? undefined : this.handleBarCodeScanned}
                                 style={StyleSheet.absoluteFillObject}/>
             )
         }
-        return(
+        return (
             <View style={styles.container}>
-            <Text style={styles.text}>
-            {hasCameraPermissions ? scannedData : "Solicitar permissão para a câmera"}
-            </Text>
-            <TouchableOpacity style={styles.button}
-                              onPress={()=> this.getCameraPermissions("scanner")}>
-            <Text style={styles.buttonText}>
-            Digitalizar QR Code
-            </Text>
-            </TouchableOpacity>
+                <ImageBackground source={bgImage} style={styles.bgImage}>
+                    <View style={styles.upperContainer}>
+                        <Image source={appIcon} style={styles.appIcon}/>
+                        <Image source={appName} style={styles.appName}/>
+                    </View>
+              <View style={styles.lowerContainer}>
+                <View style={styles.textInputContainer}>
+                    <TextInput
+                      style={styles.textInput}
+                      placeholder={"Id livro"}
+                      placeholderTextColor={"#FFFFFF"}
+                      value={bookId}
+                      />
+                    <TouchableOpacity
+                      style={styles.scanbutton}
+                      onPress={()=>this.getCameraPermissions("bookId")}
+                    >
+                    <Text style={styles.scanButtonText}>Digitalizar</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={[styles.textInputContainer,{marginTop: 25}]}>
+                    <TextInput
+                      style={styles.textInput}
+                      placeholder={"Id aluno"}
+                      placeholderTextColor={"#FFFFFF"}
+                      value={studentId}
+                      />
+                    <TouchableOpacity
+                      style={styles.scanbutton}
+                      onPress={()=>this.getCameraPermissions("studentId")}
+                    >
+                    <Text style={styles.scanButtonText}>Digitalizar</Text>
+                    </TouchableOpacity>
+                </View>
+              </View>
+              </ImageBackground>
             </View>
-        )
+          );
     }
 }
 
@@ -78,5 +120,62 @@ const styles = StyleSheet.create({
     text: {
         color: "#fff",
         fontSize: 25
-    }
+    },
+    lowerContainer: {
+        flex: 0.5,
+        alignItems: "center"
+      },
+      textInputContainer: {
+        borderWidth: 2,
+        borderRadius: 10,
+        flexDirection: "row",
+        backgroundColor: "#9DFD24",
+        borderColor: "#FFFFFF"
+      },
+      textInput: {
+        width: "57%",
+        height: 50,
+        padding: 10,
+        borderColor: "#FFFFFF",
+        borderRadius: 10,
+        borderWidth: 3,
+        fontSize: 18,
+        backgroundColor: "#5653D4",
+        fontFamily: "Rajdhani_600SemiBold",
+        color: "#FFFFFF"
+      },
+      scanbutton: {
+        width: 100,
+        height: 50,
+        backgroundColor: "#9DFD24",
+        borderTopRightRadius: 10,
+        borderBottomRightRadius: 10,
+        justifyContent: "center",
+        alignItems: "center"
+      },
+      scanButtonText: {
+        fontSize: 20,
+        color: "#0A0101",
+        fontFamily: "Rajdhani_600SemiBold"
+      },
+      bgImage: {
+        flex: 1,
+        resizeMode: "cover",
+        justifyContent: "center"
+      },
+      upperContainer: {
+        flex: 0.5,
+        justifyContent: "center",
+        alignItems: "center"
+      },
+      appIcon: {
+        width: 200,
+        height: 200,
+        resizeMode: "contain",
+        marginTop: 80
+      },
+      appName: {
+        width: 180,
+        resizeMode: "contain"
+      },
 })
